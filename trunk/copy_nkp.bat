5eeb762da5f6406ae13f461bb82fe6635fd2bca6
(no author) <(no author)@6487960e-e977-9441-a7b3-96befa83b359>
2009-09-06 22:12:23 +0000

    git-svn-id: svn://localhost/TolonSpellCheckLib@151 6487960e-e977-9441-a7b3-96befa83b359

--- trunk/copy_nkp.bat
+++ trunk/copy_nkp.bat
@@ -1,2 +1,4 @@
+mkdir .\..\..\notekeeper\trunk\usr-debug
+mkdir .\..\..\notekeeper\trunk\usr-release
 xcopy /e /y .\usr-debug .\..\..\notekeeper\trunk\usr-debug
 xcopy /e /y .\usr-release .\..\..\notekeeper\trunk\usr-release
