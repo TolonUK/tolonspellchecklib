3770366a9b009133632c000843fa88a8b3d6a6ed
(no author) <(no author)@6487960e-e977-9441-a7b3-96befa83b359>
2010-08-25 20:27:07 +0000

    Set result string to null for time being
    git-svn-id: svn://localhost/TolonSpellCheckLib@170 6487960e-e977-9441-a7b3-96befa83b359

--- trunk/test/tscTest.cpp
+++ trunk/test/tscTest.cpp
@@ -338,6 +338,8 @@ bool test_word()
     memset(&cw, 0xff, sizeof(TSC_CHECKWORD_DATA));
     cw.cbSize = sizeof(TSC_CHECKWORD_DATA);
     cw.uTestWord.szWord8 = szWordToTest;
+    cw.uResultString.szResults8 = NULL;
+    cw.nResultStringSize = 0;
     r = ::tscCheckWord(c, &cw);   
     util_is_success("tscCheckWord", r, bTestResult);
     
