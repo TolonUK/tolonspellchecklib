f673db9914f2eed6b48cc810b3049107e7ca77e8
tolon <alex@tolon.co.uk>
2014-01-25 23:35:14 +0000

    delete target file before copying

--- copy_nkp.bat
+++ copy_nkp.bat
@@ -1,9 +1,11 @@
-mkdir .\..\..\notekeeper\trunk\usr-debug
-mkdir .\..\..\notekeeper\trunk\usr-release
-xcopy /e /y .\usr-debug .\..\..\notekeeper\trunk\usr-debug
-xcopy /e /y .\usr-release .\..\..\notekeeper\trunk\usr-release
+mkdir %1\usr-debug
+mkdir %1\usr-release
+xcopy /e /y .\usr-debug %1\usr-debug
+xcopy /e /y .\usr-release %1\usr-release
 
-mkdir .\..\..\notekeeper\trunk\usr-debug\bin\enchant
-mkdir .\..\..\notekeeper\trunk\usr-release\bin\enchant
-move .\..\..\notekeeper\trunk\usr-debug\bin\libenchant_*.dll .\..\..\notekeeper\trunk\usr-debug\bin\enchant
-move .\..\..\notekeeper\trunk\usr-release\bin\libenchant_*.dll .\..\..\notekeeper\trunk\usr-release\bin\enchant
+mkdir %1\usr-debug\bin\enchant
+mkdir %1\usr-release\bin\enchant
+del %1\usr-debug\bin\enchant\libenchant_*.dll
+move %1\usr-debug\bin\libenchant_*.dll %1\usr-debug\bin\enchant
+del %1\usr-release\bin\enchant\libenchant_*.dll
+move %1\usr-release\bin\libenchant_*.dll %1\usr-release\bin\enchant
